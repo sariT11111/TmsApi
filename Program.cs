b901e4c11e91a2a841b57284bbbd471b9dd464b6
sariT11111 <tesfayesaron65@gmail.com>
2026-06-16 21:43:15 -0700

    feat(module-4/session-2): add enrollment service and worker scope

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseDefaultServiceProvider(options =>
+{
+    options.ValidateScopes = true;
+    options.ValidateOnBuild = true;
+});
+
 builder.Services.AddControllers();
 
 builder.Services
@@ -9,6 +15,9 @@ builder.Services
     .AddScheme<AuthenticationSchemeOptions, DemoAuthenticationHandler>("Demo", null);
 
 builder.Services.AddAuthorization();
+
+builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
+builder.Services.AddSingleton<EnrollmentWorker>();
 
 var app = builder.Build();
 
@@ -24,5 +33,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/api/enrollments/worker-smoke", (EnrollmentWorker worker) =>
+{
+    worker.ProcessBatch();
+    return Results.Ok("processed");
+});
 
 app.Run();
